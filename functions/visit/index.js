794bf3499416ca1fba75a36b81484a997780d4df
simmmm <dev.simdd@gmail.com>
2025-12-30 15:30:51 +0800

    Update index.js

--- functions/visit/index.js
+++ functions/visit/index.js
@@ -1,9 +1,12 @@
 export async function onRequest({ request, params, env }) {
   try {
     // request.headers.delete('accept-encoding');
-    return new Response(request.headers.get('accept-encoding'));
     
-    // return await fetch('http://scf.pages-cloud.qcdntest.cn/stream-chat');
+    return await fetch('http://scf.pages-cloud.qcdntest.cn/stream-chat'， {
+       headers: {
+        'accept-encoding': 'identity'
+      }
+    });
   } catch (err) {
     console.error(err);
     return new Response('error',
