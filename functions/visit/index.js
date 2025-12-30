9e3073b28888dfdb43911f7222f863dac18a440e
simmmm <dev.simdd@gmail.com>
2025-12-30 15:18:33 +0800

    Update index.js

--- functions/visit/index.js
+++ functions/visit/index.js
@@ -1,13 +1,11 @@
 export async function onRequest({ request, params, env }) {
   try {
-    request.headers.delete('accept-encoding');
-    return await fetch('http://scf.pages-cloud.qcdntest.cn/stream-chat');
+    // request.headers.delete('accept-encoding');
+    return new Response(request.headers.get('accept-encoding'));
+    // return await fetch('http://scf.pages-cloud.qcdntest.cn/stream-chat');
   } catch (err) {
     console.error(err);
-    return new Response(
-      JSON.stringify({
-        error: "KV storage hasn't been set up for your EdgeOne Pages Project.",
-      }),
+    return new Response('error',
       {
         headers: {
           'content-type': 'application/json; charset=UTF-8',
