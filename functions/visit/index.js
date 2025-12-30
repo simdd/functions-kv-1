065548f8c0f6e807b8183dd840c6e6725ed07e85
simmmm <dev.simdd@gmail.com>
2025-12-30 17:07:58 +0800

    Update index.js

--- functions/visit/index.js
+++ functions/visit/index.js
@@ -1,5 +1,5 @@
 export async function onRequest({ request, params, env }) {
-  const headersObj = Object.fromEntries(request.headers);
-  return new Response(JSON.stringify(headersObj));
-  // return await fetch('http://scf.pages-cloud.qcdntest.cn/stream-chat';
+  // const headersObj = Object.fromEntries(request.headers);
+  // return new Response(JSON.stringify(headersObj));
+  return await fetch('http://scf.pages-cloud.qcdntest.cn/stream-chat';
 }
