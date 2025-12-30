38a0977931e47b3345739cfa4670f8171d5cb3b8
simmmm <dev.simdd@gmail.com>
2025-12-30 15:33:45 +0800

    Update index.js

--- functions/visit/index.js
+++ functions/visit/index.js
@@ -1,21 +1,26 @@
 export async function onRequest({ request, params, env }) {
   try {
-    // request.headers.delete('accept-encoding');
-    
-    return await fetch('http://scf.pages-cloud.qcdntest.cn/stream-chat'， {
-       headers: {
-        'accept-encoding': 'identity'
-      }
+    return await fetch('http://scf.pages-cloud.qcdntest.cn/stream-chat', {
+      method: request.method,
+      headers: {
+        // 只复制需要的头，不包含 accept-encoding
+        // 或者明确设置
+        'accept-encoding': 'identity',
+        // 复制其他可能需要的头
+        ...(request.headers.get('content-type') && {
+          'content-type': request.headers.get('content-type')
+        })
+      },
+      // 如果有 body，也需要处理
+      body: request.body
     });
   } catch (err) {
     console.error(err);
-    return new Response('error',
-      {
-        headers: {
-          'content-type': 'application/json; charset=UTF-8',
-          'Access-Control-Allow-Origin': '*',
-        },
-      }
-    );
+    return new Response('error', {
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
   }
 }
