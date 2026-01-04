a1f543d6380c1abc7a93d02615fe7bf2aa308fa1
simmmm <dev.simdd@gmail.com>
2026-01-04 17:38:32 +0800

    Update index.js

--- functions/visit/index.js
+++ functions/visit/index.js
@@ -1,21 +1,10 @@
 export async function onRequest({ request, params, env }) {
-  let result = 0;
-  // 循环次数取决于机器性能。
-  // 在普通服务器上，1000万次浮点运算通常需要 100ms-300ms
-  const iterations = 20000000; // 2千万次
-
-  for (let i = 0; i < iterations; i++) {
-      // Math.sqrt 和 Math.random 都是相对昂贵的 CPU 指令
-      result += Math.sqrt(i) * Math.random();
-  }
-  
   return new Response(JSON.stringify({
     "status": true,
     "msg": "服务运行正常",
     "ext": {
       "client-ip": 'xxx',
-      "server-ip": 'xxx2',
-      "cost": result
+      "server-ip": 'xxx2'
     }
   }), {
     status: 200,
